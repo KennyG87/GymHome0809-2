aa9b3c0bd43bfdbbccb6ac909754ca315987006d
jykuo <jykuo@ntut.edu.tw>
2017-08-09 22:10:27 +0800

    0809V1

--- app/src/main/java/com/example/kennykao/gymhome0808/LoginActivity.java
+++ app/src/main/java/com/example/kennykao/gymhome0808/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.example.kennykao.gymhome0808;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -14,9 +13,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 
-
+import com.example.kennykao.gymhome_2.Common;
+import com.example.kennykao.gymhome_2.MemberCoach;
+import com.example.kennykao.gymhome_2.R;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
@@ -30,124 +32,86 @@ import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 
-
+import CoachesLogin.CoachesVO;
 import StudentsLogin.StudentsVO;
 
+import static com.example.kennykao.gymhome_2.Common.networkConnected;
+import static com.example.kennykao.gymhome_2.Common.showToast;
 
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
-    private final static String ACTION = "getAll";
-    private AsyncTask studentsLoginTask;
+    private StudentsLoginTask studentsLoginTask;
     private ProgressDialog progressDialog;
     private RadioGroup rgMembers;
     private RadioButton rbStudents;
     private RadioButton rbCoaches;
     private Button btLogin;
-    private EditText etEmail;
+    private EditText etUser;
     private EditText etPassword;
+    private TextView tvMessage;
+    private StudentsVO stus;
+    private CoachesVO coas;
+    private MemberCoach memberCoach;
 
 
-
-    private class StudentsLoginTask extends AsyncTask<String, Object, List<StudentsVO>> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-//            progressDialog = new ProgressDialog(LoginActivity.this);
-//            progressDialog.setMessage("Loading...");
-//            progressDialog.show();
-            Log.d(TAG, "ZZZZZZZZZZZZZZZZZZZZZZZZZZz: ");
-
-        }
-
-        @Override
-        protected List<StudentsVO> doInBackground(String... params) {
-            String url = params[0].toString();
-            String jsonIn;
-            JsonObject jsonObject = new JsonObject();
-            jsonObject.addProperty("action", ACTION);
-            try {
-                jsonIn = getRemoteData(url, jsonObject.toString());
-            } catch (IOException e) {
-                Log.e(TAG, e.toString());
-                return null;
-            }
-
-            Gson gson = new Gson();
-            Type listType = new TypeToken<List<StudentsVO>>() {
-            }.getType();
-            return gson.fromJson(jsonIn, listType);
-        }
-
-        private String getRemoteData(String url, String jsonOut) throws IOException {
-            StringBuilder jsonIn = new StringBuilder();
-            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setDoInput(true); // allow inputs
-            connection.setDoOutput(true); // allow outputs
-            connection.setUseCaches(false); // do not use a cached copy
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("charset", "UTF-8");
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
-            bw.write(jsonOut);
-            Log.d(TAG, "jsonOut: " + jsonOut);
-            bw.close();
-
-            int responseCode = connection.getResponseCode();
-
-            if (responseCode == 200) {
-                BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String line;
-                while ((line = br.readLine()) != null) {
-                    jsonIn.append(line);
-                }
-            } else {
-                Log.d(TAG, "response code: " + responseCode);
-            }
-            connection.disconnect();
-            Log.d(TAG, "jsonIn: " + jsonIn);
-            return jsonIn.toString();
-        }
-    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.d(TAG, "yyyyyyyyyyyyyyyyyyyyyyyyyyy: ");
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+        setResult(RESULT_CANCELED);
+        memberCoach = new MemberCoach();
         rgMembers = (RadioGroup) findViewById(R.id.rgMembers);
         rbCoaches = (RadioButton) findViewById(R.id.rbCoaches);
         rbStudents = (RadioButton) findViewById(R.id.rbStudents);
         btLogin = (Button) findViewById(R.id.btLogin);
-        etEmail = (EditText) findViewById(R.id.etEmail);
+        etUser = (EditText) findViewById(R.id.etUser);
         etPassword = (EditText) findViewById(R.id.etPassword);
-        Log.d(TAG, "yyyyyyyyyyyyyyyyyyyyyyyyyyy: ");
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        tvMessage = (TextView) findViewById(R.id.tvMessage);
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUser.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+                if (username.length() <= 0 || password.length() <= 0) {
+                    showMessage(R.string.InvalidUserOrPassword);
+                    return;
+                }
+                String role = "stu"; // from radio button
 
-//最基本的click回應
-//        btLogin.setOnClickListener(new Button.OnClickListener(){
-//            @Override
-//            public void onClick(View v){
-//                Intent intent = new Intent();
-//                intent.setClass(LoginActivity.this, HomepageActivity.class);
-//                startActivity(intent);
-//                LoginActivity.this.finish();
-//            }
-//        });
+                Object obj = null;
+                try {
+                    memberCoach = isUserValid(role, username, password);
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                stus = memberCoach.getStudentsVO();
+                coas = memberCoach.getCoachesVO();
+
+                if(memberCoach == null ) {
+                    showToast(getBaseContext(), "WRONG");
+                    return;
+                }else {
+                    showToast(getBaseContext(), "Ok");
+
+                }
 
 
-//        btLogin.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//                String email = etEmail.getText().toString().trim();
-//                String password = etPassword.getText().toString().trim();
-//                if (email.length() <= 0 || password.length() <= 0) {
-//                    showMessage(R.string.msg_InvalidEmailOrPassword);
-//                    return;
-//                }
-//
-//                if (isLoginValid(email, password)) {
+
+
+
+
+//                if (isUserValid(email, password)) {
 //                    SharedPreferences pref = getSharedPreferences(Common.PREF_FILE,
 //                            MODE_PRIVATE);
 //                    pref.edit()
@@ -160,32 +124,100 @@ public class LoginActivity extends AppCompatActivity {
 //                } else {
 //                    showMessage(R.string.msg_InvalidEmailOrPassword);
 //                }
-//            }
+            }
+        });
+
+
+
+
+    }
+
+
+    class StudentsLoginTask extends AsyncTask<String, Object, MemberCoach> {
+
+
+//        @Override
+//        protected void onPreExecute() {
+//            super.onPreExecute();
+//            progressDialog = new ProgressDialog(LoginActivity.this);
+//            progressDialog.setMessage("Loading...");
+//            progressDialog.show();
+//            Log.d(TAG, "ZZZZZZZZZZZZZZZZZZZZZZZZZZz: ");
 //
+//        }
 //
-//        });
-//    }
-//
-//    private void showMessage(int msg_invalidEmailOrPassword) {
-//        return;
-//    }
-//
-//
-//
-//    private boolean isLoginValid(String email, String password) {
-//        return Boolean.parseBoolean(null);
-//    }
-//
+
+        @Override
+        protected MemberCoach doInBackground(String... params) {
+            String role = params[1];
+            String url = params[0];
+            String username = params[2];
+            String password = params[3];
+            String jsonIn;
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("role", role);
+            jsonObject.addProperty("username", username);
+            jsonObject.addProperty("password", password);
+
+            try {
+                jsonIn = getRemoteData(url, jsonObject.toString());
+            } catch (IOException e) {
+                Log.e(TAG, e.toString());
+                return null;
+            }
+
+            Gson gson = new Gson();
+//            Type listType = new TypeToken<MemberCoach>() {
+//            }.getType();
+//            Object obj = gson.fromJson(jsonIn,Object.class);
+            return  gson.fromJson(jsonIn, MemberCoach.class);
+
+        }
+
+    }
+
+    private String getRemoteData(String url, String jsonOut) throws IOException {
+        StringBuilder jsonIn = new StringBuilder();
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        connection.setDoInput(true); // allow inputs
+        connection.setDoOutput(true); // allow outputs
+        connection.setUseCaches(false); // do not use a cached copy
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("charset", "UTF-8");
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+        bw.write(jsonOut);
+        Log.d(TAG, "jsonOut: " + jsonOut);
+        bw.close();
+
+        int responseCode = connection.getResponseCode();
+        jsonIn = new StringBuilder();
+        if (responseCode == 200) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = br.readLine()) != null) {
+                jsonIn.append(line);
+            }
+        } else {
+            Log.d(TAG, "response code: " + responseCode);
+        }
+        connection.disconnect();
+        Log.d(TAG, "jsonIn: " + jsonIn);
+        return jsonIn.toString();
+    }
+
+
+
+    //    }
 //
 //    @Override
 //    protected void onStart() {
-//        super.onStart();
+//        progressDialog.onStart();
 //        SharedPreferences pref = getSharedPreferences(Common.PREF_FILE, MODE_PRIVATE);
 //        boolean login = pref.getBoolean("login", false);
 //        if (login) {
-//            String email = pref.getString("email", "");
+//            String name = pref.getString("email", "");
 //            String password = pref.getString("password", "");
-//            if (isLoginValid(email, password)) {
+//            if (isUserValid(name, password)) {
 //                setResult(RESULT_OK);
 //                finish();
 //            } else {
@@ -194,15 +226,40 @@ public class LoginActivity extends AppCompatActivity {
 //        }
 //    }
 //
-//
-//
-//    private boolean isLoginValid(String email, String password) {
-//        // 應該連線至server端檢查帳號密碼是否正確
-//        return email.equals("a");
-//    }
+    private void showMessage(int msgResId) {
+        tvMessage.setText(msgResId);
+    }
+
+    private MemberCoach isUserValid(String role, String username, String password) throws ExecutionException, InterruptedException {
+        Object obj = null;
+        if (networkConnected(this)) {
+            if (studentsLoginTask == null){
+                studentsLoginTask = new StudentsLoginTask();
+            }
+            memberCoach =studentsLoginTask.execute(Common.URL, role, username, password).get();
+        } else {
+            showToast(this, R.string.tryagain);
+        }
+        return memberCoach;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (studentsLoginTask != null){
+            studentsLoginTask.cancel(true);
+            studentsLoginTask = null;
+        }
+    }
+}
 
 
-}}
+
+
+
+
+
+
 
 
 
